da6561fc7e90ffa414c15f77c7181374479c4edc
saul-greyna <saulreynag.07@gmail.com>
2024-08-12 23:14:46 -0600

    main

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('.menu').classList.toggle('show');
+document.querySelector('.menu').classList.toggle('show');
 });
